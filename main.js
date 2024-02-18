f74c43eba1495c775fb6d1b5f60ff0964eabafb3
Pratyush615 <75452860+Pratyush615@users.noreply.github.com>
2024-02-18 10:41:52 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+let latitude = 22.7868542, longitude = 88.3643296;
+
+// Initializing Mapbox
+mapboxgl.accessToken = 'pk.eyJ1IjoicG9vamEtMTEiLCJhIjoiY2xrc3BwcmptMDFyNzNubnBxbGxqcjAzMyJ9.hXgOsamHpjJJVXAFAw_fsQ'
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [longitude,latitude],
+    zoom: 16
+})
+
+
+
+//Add Mapbox Geocoder
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions:{
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    })
+)
+
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl:mapboxgl
+    })
+)
+
+
+
+
+// Create a Amber Palace, Jaipur Marker and add it to the map.
+var img1 = document.querySelector('#amber')
+
+var marker1 = new mapboxgl.Marker({
+    element:img1
+})
+.setLngLat([75.85133,26.98547])
+.addTo(map)
+// Create a  Gateway of India, Mumbai Marker and add it to the map.
+var img2 = document.querySelector('#gateway')
+
+var marker2 = new mapboxgl.Marker({
+    element:img2
+})
+.setLngLat([72.834641,18.922064]) 
+.addTo(map)
+// Create a India Gate Marker and add it to the map.
+var img3 = document.querySelector('#gate')
+
+var marker3 = new mapboxgl.Marker({
+    element:img3
+})
+.setLngLat([77.2295,28.6129])
+.addTo(map)
+// Create a Lotus Temple, Delhi Marker and add it to the map.
+var img4 = document.querySelector('#temple')
+
+var marker4 = new mapboxgl.Marker({
+    element:img4
+})
+.setLngLat([77.2588,28.5535])
+.addTo(map)
+//Create a Victoria Memorial, Kolkata Marker and add it to the map.
+var img5 = document.querySelector('#victoria')
+
+var marker5 = new mapboxgl.Marker({
+    element:img5
+})
+.setLngLat([88.3426,22.5448])
+.addTo(map)
